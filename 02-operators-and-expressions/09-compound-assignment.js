76a23103bbc65f58f6f7bd7c4101acc92c784348
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-03-21 12:16:24 -0300

    feat: compound assignment

---
+++ 02-operators-and-expressions/09-compound-assignment.js
@@ -0,0 +1,9 @@
+var balance = 100;
+
+console.log(balance += 50); // 150
+console.log(balance -= 20); // 130
+console.log(balance *= 2); // 260
+console.log(balance /= 4); // 65
+console.log(balance %= 7); // 2 
+
+// The compound assignment operators (+=, -=, *=, /=, %=) perform an operation on the variable and then assign the result back to that variable. For example, balance += 50 is equivalent to balance = balance + 50.
