e726453a0024c8a2a20e6c7758d7410b37481b4c
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-03-23 18:39:50 -0300

    feat: armstrong number checker

---
+++ 03-control-flow-and-loops/07-armstrong-number-checker.js
@@ -0,0 +1,18 @@
+// An Armstrong number (like 153) is a number that is the sum of its own digits each raised to the power of the number of digits (1^3 + 5^3 + 3^3 = 153). Write a script to check if a 3-digit number is an Armstrong number. 
+
+const num = 153;
+let tempNum = num;
+let sumOfPowers = 0;
+
+const numDigits = [];
+for (let temp = num; temp > 0; temp = Math.floor(temp / 10)) {
+  numDigits.unshift(temp % 10);
+}
+
+numDigits.forEach(digit => {
+  sumOfPowers += Math.pow(digit, numDigits.length);
+})
+
+console.log(`Number: ${num}`);
+console.log(`Digits: ${numDigits.join(", ")}`);
+console.log(`Sum of digits raised to the power of ${numDigits.length}: ${sumOfPowers}`);
